90aca5e5528c801546b592977f4ed444a9aca152
Олег Цыплаков <olegtsy@mail.ru>
2023-12-20 23:44:42 +0400

    add json propertty to airport response

--- Contracts/Responses/AirportLocationResponse.cs
+++ Contracts/Responses/AirportLocationResponse.cs
@@ -1,8 +1,13 @@
-﻿namespace Sirena.Api.Contracts.Responses
+﻿using Newtonsoft.Json;
+
+
+namespace Sirena.Api.Contracts.Responses
 {
     public class AirportLocationResponse
     {
+        [JsonProperty("lon")]
         public double Longitude { get; set; }
+        [JsonProperty("lat")]
         public double Latitude { get; set; }
     }
 }
--- Contracts/Responses/AirportResponse.cs
+++ Contracts/Responses/AirportResponse.cs
@@ -1,17 +1,31 @@
-﻿namespace Sirena.Api.Contracts.Responses
+﻿using Newtonsoft.Json;
+
+
+namespace Sirena.Api.Contracts.Responses
 {
     public class AirportResponse
     {
+        [JsonProperty("iata")]
         public string Iata { get; set; } = string.Empty;
+        [JsonProperty("name")]
         public string Name { get; set; } = string.Empty;
+        [JsonProperty("city")]
         public string City { get; set; } = string.Empty;
+        [JsonProperty("city_iata")]
         public string CityIata { get; set; } = string.Empty;
+        [JsonProperty("country")]
         public string Country { get; set; } = string.Empty;
+        [JsonProperty("country_iata")]
         public string CountryIata { get; set; } = string.Empty;
+        [JsonProperty("location")]
         public AirportLocationResponse Location { get; set; }
+        [JsonProperty("rating")]
         public int Rating { get; set; }
+        [JsonProperty("hubs")]
         public int Hubs { get; set; }
+        [JsonProperty("timezone_region_name")]
         public string TimezoneRegionName { get; set; } = string.Empty;
+        [JsonProperty("type")]
         public string Type { get; set; } = string.Empty;
     }
 }
